6a5bf43b3f3de23940f94c5e254c450ff1586ca3
weebdesign <155826765+weebdesign@users.noreply.github.com>
2024-03-07 09:52:53 +0600

    Add files via upload

---
+++ resoucese/js/custom.js
@@ -0,0 +1,29 @@
+
+
+        $(document).ready (function(){
+
+// For  sticky position
+
+$('.button-parent').click(function(){
+    $('.role').hide();
+});
+
+
+
+    // miixitup for [portfolio section]
+    var mixer = mixitup(".container");
+
+    
+   
+        
+    });
+    
+    $(".portfolio-section").ripples({
+        resulation:512,
+        dropredius:20,
+        perturbance:0.09,
+    });
+
+
+
+   
